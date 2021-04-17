c4959da350dfae387b7e763bde8b88a382550d6e
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-18 00:45:36 +0530

    creation of linked list has been added

---
+++ creation of linked list/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node {
+    int data;
+    struct node *next;
+};
+void main()
+{
+    struct node *head,*second,*third;
+    head=(struct node*)malloc(sizeof(struct node));
+    second()
+
+    
+    printf();
+}
