8023ebba988431a87ea9ece574acadd954bce50c
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-18 21:09:21 +0530

    creation of node has been added

---
+++ creation of n nodes/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node{
+    int data;
+    struct node *link;
+};
+void main()
+{
+  struct node *temp,*root,*p; //initially the value of all these variable is null
+  int n,i;
+  printf("enter the number of nodes: ");
+  scanf("%d",&n);
+  temp=(struct node*)malloc(sizeof(struct node ));  
+  for(i=1;i<=n;i++)
+  {
+      printf("enter the %d element into the node: ",i);
+      scanf("%d",&temp->data);
+      temp->link=NULL;
+      if(root==NULL)
+      {
+          root=temp;
+      }
+
+  }
+}
