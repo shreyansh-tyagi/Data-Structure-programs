78f98a05abff7f95551685ffad0096ab205b4518
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-20 02:02:08 +0530

    insertion of one linked list into another

--- insertion of linked list into another linked list/main.c
+++ insertion of linked list into another linked list/main.c
@@ -34,6 +34,7 @@ void main()
       }
 
   }
+  printf("\nfirst node:\n\n");
   while(root->link!=NULL){
       printf("%d  ",root->data);
       root=root->link;
@@ -43,10 +44,38 @@ void main()
 }
 struct node * insert(struct node *root,int n)
 {
-    struct node *temp;
+    struct node *temp=NULL,*p=NULL,*ptr=NULL,*linknode=NULL;
     int m,i;
     printf("enter the size of new node: ");
     scanf("%d",&m);
+    for(i=1;i<=m;i++)
+    {
     temp=(struct node*)malloc(sizeof(struct node));
+    printf("enter the data into %d node of new linked list: ",i);
+    scanf("%d",&temp->data);
+    temp->link=NULL;
+    if(p==NULL)
+    {
+        p=temp;
+        linknode=temp;
+    }
+    else{
+        ptr=p;
+        while(ptr->link!=NULL)
+        {
+            ptr=ptr->link;
+        }
+        ptr->link=temp;
+
+    }
+    printf("\n\nsecond node:\n\n");
+    while(linknode->link!=NULL)
+    {
+        printf("%d  ",linknode->data);
+    }
+    printf("%d",linknode->data);
+
+
+    }
 
 }
