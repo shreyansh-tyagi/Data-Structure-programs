5e4aa1571d55a52b46e42151a39f447a2d0b83a7
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-21 13:35:17 +0530

    insertion of one linked list into another

--- insertion of linked list into another linked list/main.c
+++ insertion of linked list into another linked list/main.c
@@ -9,7 +9,7 @@ struct node {
 struct node* insert(struct node*,int);
 void main()
 {
-  struct node *temp=NULL,*root=NULL,*p=NULL;
+  struct node *temp=NULL,*root=NULL,*p=NULL,*root1=NULL;
   int n,i;
   printf("\nenter the size of nodes: ");  
   scanf("%d",&n);
@@ -22,6 +22,7 @@ void main()
       temp->link=NULL;
       if(root==NULL){
           root=temp;
+          root1=temp;
       }
       else{
           p=root;
@@ -35,16 +36,16 @@ void main()
 
   }
   printf("\nfirst node:\n\n");
-  while(root->link!=NULL){
-      printf("%d  ",root->data);
-      root=root->link;
+  while(root1->link!=NULL){
+      printf("%d  ",root1->data);
+      root1=root1->link;
   }
-  printf("%d",root->data);
+  printf("%d",root1->data);
   root=insert(root,n);
 }
 struct node * insert(struct node *root,int n)
 {
-    struct node *temp=NULL,*p=NULL,*ptr=NULL,*linknode=NULL,*temp1=NULL,*root1=NULL;
+    struct node *temp=NULL,*p=NULL,*ptr=NULL,*linknode=NULL,*temp1=NULL,*root1=NULL,*ptr=NULL;
     root1=root;
     int m,i,pos;
     printf("\n\nenter the size of new node: ");
@@ -59,6 +60,7 @@ struct node * insert(struct node *root,int n)
     {
         p=temp;
         linknode=temp;
+        ptr=temp;
     }
     else{
         ptr=p;
@@ -79,24 +81,42 @@ struct node * insert(struct node *root,int n)
     printf("%d",linknode->data);
     printf("\nenter the position from where you want to insert the new node to existing node: ");
     scanf("%d",&pos);
+    if(pos>1)
+    {
+    pos=pos-1;
     for(i=1;i<pos;i++)
     {
-        root=root->link;
+        root1=root1->link;
     }
-    temp1=root->link;
-    root->link=p;
+    temp1=root1->link;
+    root1->link=p;
     while(p->link!=NULL)
     {
         p=p->link;
     }
-    p=temp1;
+    p->link=temp1;
     printf("\n\nafter insertion:\n\n");
-    while(root1->link!=NULL)
+    while(root->link!=NULL)
     {
-        printf("%d  ",root1->data);
-        root1=root1->link;
+        printf("%d  ",root->data);
+        root=root->link;
     }
-    printf("%d",root1->data);
+    printf("%d",root->data);
+    }
+    else if(pos==1)
+    {
+        while(p->link!=NULL)
+        {
+            p=p->link;
+        }
+        p->link=root1;
+        while(ptr->link!=NULL)
+        {
+            printf("%d  ",ptr->data);
+            ptr=ptr->link;
+        }
+        
+    }
    
 
 }
