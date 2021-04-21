5cf6b7176164ceef28e5d0869ab4347925e7900a
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-21 23:14:16 +0530

    deletion of node from linked list

--- deletion of node from linked list/main.c
+++ deletion of node from linked list/main.c
@@ -3,10 +3,20 @@
 #include<string.h>
 struct node {
     int data;
-    
-}
+    struct node *link;
+};
 void main()
 {
-    
+  struct node *temp=NULL,*root=NULL;
+  int n,i;
+  printf("\nenter the size of linked list: ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+      printf("enter the %d element into linked list: ");
+      scanf("%d",&temp->link);
+  }
+
+
     printf();
 }
