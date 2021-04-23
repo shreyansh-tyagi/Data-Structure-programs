65bf1c0ac8ea5ea621d1ccbb0b7344cde6c50f3d
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-23 22:26:49 +0530

    deletion of node from linked list

--- deletion of more than one node/main.c
+++ deletion of more than one node/main.c
@@ -6,5 +6,19 @@ struct node {
 };
 void main()
 {
-    
+    struct node *temp=NULL,*root=NULL,*P=NULL;
+    int n,i;
+    printf("enter the size of linked list: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        temp=(struct node*)malloc(sizeof(struct node));
+        printf("enter the %d element into linked list: ",i);
+        scanf("%d",&temp->data);
+        temp->link=NULL;
+        if(root->link==NULL)
+        {
+            
+        }
+    }
 }
