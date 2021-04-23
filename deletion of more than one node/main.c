6db483cfc1b5628db11b363b66130e545412f3bf
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-24 00:18:54 +0530

    deletion of one node from anywhere in the linked list

--- deletion of more than one node/main.c
+++ deletion of more than one node/main.c
@@ -52,22 +52,45 @@ struct node * display(struct node *root)
 }
 struct node * onedelete(struct node *root,int n)
 {
-    int pos,i;
+    int pos,i,m;
     struct node *temp=NULL,*p=NULL,*q=NULL;
     temp=root;
     p=root;
     q=root;
     printf("\n\enter the position from where you want to delete the node: ");
     scanf("%d",&pos);
+    if(pos>1){
+    m=pos;
     pos=pos-1;
     for(i=1;i<pos;i++)
     {
         p=p->link;
     }
-    p=p->link;
-    for(i=1;i<=pos;i++)
+    for(i=1;i<=m;i++)
     {
         q=q->link;
+    }
+    p->link=q;
+    printf("after deletion from %d position",pos);
+    while(temp!=NULL)
+    {
+        printf("%d  ",temp->data);
+        temp=temp->link;
+    }
+    printf("%d",temp->data);
+
+    }
+    else if(pos==1)
+    {
+        temp=temp->link;
+        printf("after deletion from %d position",pos);
+        while(temp!=NULL)
+        {
+           printf("%d  ",temp->data);
+           temp=temp->link;
+        }
+        printf("%d",temp->data);
+
     }
 
 
