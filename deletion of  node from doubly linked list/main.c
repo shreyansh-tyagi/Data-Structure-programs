bf7527131f6f9d565e7fb023c5be078517e20bfe
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-02 22:38:43 +0530

    deletion of node from doubly linked list

--- deletion of  node from doubly linked list/main.c
+++ deletion of  node from doubly linked list/main.c
@@ -8,6 +8,8 @@ struct node {
 };
 void display(struct node *,int);
 struct node * delete_at_begin(struct node *,int );
+void delete_at_end(struct node *,int );
+void delete_at_both_begin_end(struct node *,int );
 void main()
 {
     int n,i;
@@ -50,6 +52,8 @@ void display(struct node *root,int n)
     }
     printf("%d",temp->data);
     root=delete_at_begin(root,n);
+    delete_at_end(root,n);
+    delete_at_both_begin_end(root,n);
 
 }
 struct node * delete_at_begin(struct node *root ,int n)
@@ -58,7 +62,7 @@ struct node * delete_at_begin(struct node *root ,int n)
     temp=root->right;
     q=root->right;
     root->left=NULL;
-    printf("\n\nforward traversing After deletion:\n\n");
+    printf("\n\nAfter deletion from begining:\n\n");
     while(temp->right!=NULL)
     {
         printf("%d  ",temp->data );
@@ -66,13 +70,24 @@ struct node * delete_at_begin(struct node *root ,int n)
 
     }
     printf("%d",temp->data);
-    printf("\n\nbackward traversing After deletion:\n\n");
-    while(temp->left!=NULL)
+    return q;
+}
+void delete_at_end(struct node *root,int n)
+{
+    printf("\n\nAfter deletion from end:\n\n");
+    while(root->right!=NULL)
+    {
+        printf("%d  ",root->data);
+        root=root->right;
+    }
+}
+void delete_at_both_begin_end(struct node *root,int n)
+{
+    struct node *temp=NULL;
+    temp=root->right;
+    printf("\n\nAfter deletion from both begining and end:\n\n");
+    while(temp->right!=NULL)
     {
         printf("%d  ",temp->data);
-        temp=temp->left;
-
     }
-    printf("%d",temp->data);
-    return q;
 }
