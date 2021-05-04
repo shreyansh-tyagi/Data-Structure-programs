6754d9a3c477a88e96e409c2a605cefef61ce958
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-04 12:50:12 +0530

    sorting of doubly linked list

--- sorting in double linked list/main.c
+++ sorting in double linked list/main.c
@@ -61,5 +61,83 @@ void display(struct node *root,int n)
 }
 void sorting(struct node *root,int n)
 {
+    struct node *temp=NULL,*p=NULL,*q=NULL;
+    int i,j,temp1,temp2,m,a;
+    printf("\n\nChoose from the menu:\n1. Ascending order sort, press '1'\n2. Descending order sort, press '2'\n3. To exit, press '0': ");
+    scanf("%d",&a);
+    q=root;
+    m=n;
+    switch(a)
+    {
+        case 1:
+        {
+        printf("\n\nAfter sorting in Ascending order:\n\n");
+        for(i=1;i<=n;i++)
+        {
+        temp=root;
+        p=root;
+        for(j=1;j<m;j++)
+        {
+            temp1=temp->data;
+            p=temp->right;
+            temp2=p->data; 
+            if(temp1>temp2)
+            {
+                temp->data=temp2;
+                p->data=temp1;
+            }
+            temp=temp->right;
+        }
+    }
+    while(q->right!=NULL)
+    {
+        printf("%d  ",q->data);
+        q=q->right;
+    }
+    printf("%d",q->data);
+    break;
+
+        }
+
+    case 2:
+    {
+        printf("\n\nAfter sorting in Descending order:\n\n");
+    for(i=1;i<=n;i++)
+    {
+        temp=root;
+        p=root;
+        for(j=1;j<m;j++)
+        {
+            temp1=temp->data;
+            p=temp->right;
+            temp2=p->data; 
+            if(temp1<temp2)
+            {
+                temp->data=temp2;
+                p->data=temp1;
+            }
+            temp=temp->right;
+        }
+    }
+    while(q->right!=NULL)
+    {
+        printf("%d  ",q->data);
+        q=q->right;
+    }
+    printf("%d",q->data);
+    break;
+    }  
+    case 0:
+    {
+        printf("\n\nthanks for giving your time ...goodbye!");
+        exit(1);
+    }
+    default:
+    {
+        printf("\n\nplease enter the valid number from the menu...");
+    }
+
+
+    }
     
 }
