e09ae5c3c50d57bc9304b23454b0762b7ba6ae58
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-06 21:27:32 +0530

    addition of polynomial

--- addition of polynomials/main.c
+++ addition of polynomials/main.c
@@ -9,5 +9,13 @@ void display(struct node *,int );
 void main()
 {
     int n,i;
-    printf();
+    struct node *root=NULL,*temp=NULL;
+    printf("\n\nenter the size of linked list:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        temp=(struct node *)malloc(sizeof(struct node ));
+        printf("\n\nenter the coefficient:");
+        scanf("%d",&temp->cof);
+    }
 }
