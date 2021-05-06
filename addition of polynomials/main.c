bc78b7bed72b09bd666781d002440f779d3370f1
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-06 21:23:31 +0530

    addition of polynomial

---
+++ addition of polynomials/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node {
+    int cof,exp;
+    struct node link;
+};
+void display(struct node *,int );
+void main()
+{
+    int n,i;
+    printf();
+}
