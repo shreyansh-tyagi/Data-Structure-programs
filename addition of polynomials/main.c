f0fc5a48be4bdd09c91d8c93b5e6c7c33825c5b2
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-06 21:42:31 +0530

    addition of polynomial

--- addition of polynomials/main.c
+++ addition of polynomials/main.c
@@ -3,7 +3,7 @@
 #include<string.h>
 struct node {
     int cof,exp;
-    struct node link;
+    struct node *link;
 };
 void display(struct node *,int );
 void main()
@@ -25,8 +25,25 @@ void main()
             root=temp;
         }
         else{
-            p=temp;
-            while()
+            p=root;
+            while(p->link!=NULL)
+            {
+                p=p->link;
+            }
+            p->link=temp;
         }
     }
+    display(root,n);
+}
+void display(struct node *root ,int n)
+{
+    struct node *temp=NULL;
+    temp=root;
+    printf("\n\nfirst polynomial equation:\n\n");
+    while(temp->link!=NULL)
+    {
+        printf("%dx^%d  ",temp->cof,temp->exp);
+        temp=temp->link;
+    }
+    printf("%dx^%d",temp->cof,temp->exp);
 }
