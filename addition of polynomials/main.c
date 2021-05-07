a13c2587bd9083b48ee21d5c821852ed4d2f82c5
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-07 23:33:42 +0530

    addition of polynomials

--- addition of polynomials/main.c
+++ addition of polynomials/main.c
@@ -96,7 +96,7 @@ void addition(struct node *root,struct node *root1,int n,int m)
         {
             temp->cof=root1->cof;
             temp->exp=root1->exp;
-            root1=root->link;
+            root1=root->link
         }
 
     }
