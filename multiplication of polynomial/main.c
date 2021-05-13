be7b83c526af548177836f5473882c65229aa1e0
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-13 19:04:24 +0530

    multiplication of polynomial

---
+++ multiplication of polynomial/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node {
+    int cof,exp;
+    struct node *link;
+};
+void main()
+{
+  struct node *temp=NULL,*root=NULL,*p=NULL,*q=NULL;  
+    printf();
+}
