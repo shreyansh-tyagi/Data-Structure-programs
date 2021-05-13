d2fc9d8fc9f4b0277e39d1c587e3ef782ab06e64
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-13 19:35:37 +0530

    multiplication of polynomial

--- multiplication of polynomial/main.c
+++ multiplication of polynomial/main.c
@@ -5,11 +5,12 @@ struct node {
     int cof,exp;
     struct node *link;
 };
+void display(struct node *,struct node *,int,int);
 void main()
 {
-  struct node *temp=NULL,*root=NULL,*p=NULL,*q=NULL;  
+  struct node *temp=NULL,*root=NULL,*p=NULL,*temp1=NULL,*root1=NULL,*p1=NULL;  
   int n,m,i;
-    printf("\n\nenter the size of first linked list: ");
+    printf("\n\nenter the size of first polynomial: ");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
@@ -32,4 +33,28 @@ void main()
             p->link=temp;
         }
     }
+    printf("\n\nenter the size of second polynomial: ");
+    scanf("%d",&m);
+    for(i=1;i<=m;i++)
+    {
+        temp1=(struct node*)malloc(sizeof(struct node ));
+        printf("enter the %d coefficient into first linked list: ");
+        scanf("%d",&temp1->cof);
+        printf("\nenter the %d exponent into first linked list: ");
+        scanf("%d",&temp1->exp);
+        temp1->link=NULL;
+        if(root1==NULL)
+        {
+            root1=temp1;
+        }
+        else{
+            p1=root1;
+            while(p1->link!=NULL)
+            {
+                p1=p1->link;
+            }
+            p1->link=temp1;
+        }
+    }
+    display(root,root1,n,m);
 }
