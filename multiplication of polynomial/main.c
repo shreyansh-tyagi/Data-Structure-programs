b993920814cccb5931fe08e0ca93cd07066487ce
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-15 02:53:15 +0530

    multiplication of polynomial

--- multiplication of polynomial/main.c
+++ multiplication of polynomial/main.c
@@ -147,7 +147,7 @@ struct node * complete_insert(int a,int b)
 }
 void complete_mul_add(struct node *root)
 {
-    struct node *p=NULL,*q=NULL;
+    struct node *p=NULL,*q=NULL,*r=NULL;
     q=root;
     while(root!=NULL)
     {
