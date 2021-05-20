93485dbb494717f4a39a3b7e1e1ccbea34f2358e
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-21 00:53:23 +0530

    circular linked list

---
+++ circular single linked list/main.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node {
+    int data;
+    struct node * link;
+};
+void display(struct node *);
+void main()
+{
+    struct node *temp=NULL,*root=NULL,*p=NULL,*q=NULL;
+    int n,i;
+    printf("\n\nenter the size of linked list: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        temp=(struct node *)malloc(sizeof(struct node ));
+        printf("enter the %d element into linked list: ",i);
+        scanf("%d",&temp->data);
+        temp->link=NULL;
+        if(root==NULL)
+        {
+            root=temp;
+        }
+        else{
+            p=root;
+            while(p->link!=NULL)
+            {
+                p=p->link;
+            }
+            p->link=temp;
+        }
+    }
+    q=root;
+    while(q->link!=NULL)
+            {
+                q=q->link;
+            }
+            q->link=root;
+    display(root);
+}
+void display(struct node *root)
+{
+    // 1st way of displaying the circular list item
+    struct node *temp=NULL;
+    temp=root->link;
+    printf("\n\none way linked list is:\n\n");
+    printf("%d  ",root->data);
+    while(temp!=root)
+    {
+        printf("%d  ",temp->data);
+        temp=temp->link;
+    }
+    // 2nd way of displaying the circular list item
+    // here from above temp come back to root after completion of travesing
+    printf("\n\nanother way of linked list is:\n\n");
+    while(temp->link!=root)
+    {
+        printf("%d  ",temp->data);
+        temp=temp->link;
+    }
+    printf("%d",temp->data);
+}
