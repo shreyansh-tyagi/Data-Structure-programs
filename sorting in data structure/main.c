0c6f4cc850b18b681db3814d44307b86d1acd34a
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-20 14:28:29 +0530

    sorting in data structure

--- sorting in data structure/main.c
+++ sorting in data structure/main.c
@@ -23,5 +23,20 @@ void main()
 }
 void selection_sort(int a[],int n)
 {
-
+    int i ,j,min,temp;
+    for(i=0;i<n-1;i++)
+    {
+    min=i;
+    for(j=i+1;j<n;j++)
+    {
+        if(a[j]<a[min])
+        {
+            min=j;
+        }
+    }
+    temp=a[i];
+    a[i]=a[min];
+    a[min]=temp;
+    }
+    
 }
