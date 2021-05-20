2b34baeb8b0594e0c3e595ddf76978f478a82ede
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-20 14:38:09 +0530

    sorting in data structure

--- sorting in data structure/main.c
+++ sorting in data structure/main.c
@@ -48,9 +48,9 @@ void selection_sort(int a[],int n)
 void bubble_sort(int a[],int n)
 {
     int i,j,temp;
-    for(i=0;i<=n;i++)
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<=n;j++)
+        for(j=0;j<n;j++)
         {
             if(a[i]>a[j])
             {
