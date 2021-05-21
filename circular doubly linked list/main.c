b7a58d82be6a2df0ca4e725afea6fb82a960abf0
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-21 17:38:44 +0530

    circular doubly linked list

--- circular doubly linked list/main.c
+++ circular doubly linked list/main.c
@@ -8,6 +8,12 @@ struct node {
 void display(struct node *);
 void main()
 {
-    
-    printf();
+    struct node *temp=NULL,*p=NULL,*q=NULL,*root=NULL;
+    int i,n;
+    printf("\n\nenter the size of linked list: ");
+    scanf("%d",&n);
+    for(i=1,i<=n;i++)
+    {
+        
+    }
 }
