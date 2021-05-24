0f35a3565d31b1e29451b89b0db8e78e04ed9e33
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-24 10:42:23 +0530

    stack insertion and deletion

--- stack insertion and deletion/main.c
+++ stack insertion and deletion/main.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void push(int);
+int  push(int);
 void main()
 {
-    int i,n,a[50];
+    int i,n,ele,top=-1;
     printf("\n\nenter the size of stack: ");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         printf("enter the %d value into stack: ");
-        scanf
+        scanf("%d",&ele);
+        push(ele);
     }
 
 }
