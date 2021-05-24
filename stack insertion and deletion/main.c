042bfed33f37f3e21f602777bae0dcf95990260c
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-24 10:39:36 +0530

    stack insertion and deletion

---
+++ stack insertion and deletion/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void push(int);
+void main()
+{
+    int i,n,a[50];
+    printf("\n\nenter the size of stack: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("enter the %d value into stack: ");
+        scanf
+    }
+
+}
