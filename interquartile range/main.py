36deadee030999bd17d12654f6e16fcaedeee9f8
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-06-02 22:23:16 +0530

    inter quartile range question

--- interquartile range/main.py
+++ interquartile range/main.py
@@ -14,7 +14,7 @@ for i in range(f):
 for j in range(f,len(d)):
     e.append(d[j])
 g,h=round(statistics.median(c),1),round(statistics.median(e),1)
-print(round((h-g),1))
+print(round(float(h-g),1))
 
     
     
