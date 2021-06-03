9301bdcfcfdcc31d4f4bc392afa95f878bf7be4f
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-06-03 20:44:21 +0530

    stack push pop using linked list

--- stack push pop using linked list/main.c
+++ stack push pop using linked list/main.c
@@ -11,6 +11,7 @@ void main()
 {
     int ele,n;
     printf("\n----choose from the choice from the menu---- \n");
+    printf("\n1. push the element into stack\n2. pop the element from the stack")
     scanf("%d",&n);
     switch(n)
     {
